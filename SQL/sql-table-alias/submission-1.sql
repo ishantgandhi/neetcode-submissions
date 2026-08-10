8480a0adbc8ba02c28ba2647b0eb94be792db577
Ishant Gandhi <126364217+ishantgandhi@users.noreply.github.com>
2026-08-09 20:34:04 -0700

    Add: sql-table-alias - submission-1

---
+++ SQL/sql-table-alias/submission-1.sql
@@ -0,0 +1,31 @@
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    email TEXT
+);
+
+CREATE TABLE employment_records (
+    id INTEGER PRIMARY KEY,
+    user_id INTEGER REFERENCES users(id),
+    company_name TEXT,
+    job_title TEXT
+);
+
+
+INSERT INTO users (id, name, email) VALUES
+(1, 'Alice', 'alice@gmail.com'),
+(2, 'Bob', 'bob@gmail.com'),
+(3, 'Charlie', 'charlie@gmail.com');
+
+INSERT INTO employment_records (id, user_id, company_name, job_title) VALUES
+(100, 1, 'Google', 'Software Engineer'),
+(300, 2, 'Microsoft', 'Data Scientist'),
+(500, 3, 'Amazon', 'Product Manager');
+-- Do not modify above this line. --
+
+
+select name,email,company_name, job_title from users as u
+inner join employment_records as e on u.id = e.user_id order by company_name;
+
+
+
